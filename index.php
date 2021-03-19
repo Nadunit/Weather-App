4ed74ac648fad093f8bb923ee9287087e6eb8078
Nadunit <33655132+Nadunit@users.noreply.github.com>
2021-03-19 10:36:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-  echo "welcome";
+  
  ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+
+     <link rel="stylesheet" href="indexstyle.css">
+   </head>
+   <body>
+     <button type="button" name="button"><a href="login.php">Click here to continue</a> </button>
+   </body>
+ </html>
